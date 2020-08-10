f37e169e5f4aa31715ba60a9196e647a04c277e8
EricksonLopes <ofc.erickson@gmail.com>
2020-08-10 17:52:56 -0300

    Boas práticas de serialização com relacionamentos

--- api/serializers.py
+++ api/serializers.py
@@ -4,15 +4,31 @@ from rest_framework import serializers
 from api.models import Category
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        # fields = '__all__'
+
+
 class CategorySerializer(serializers.ModelSerializer):
     # id do usuario
     # user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
 
     # nome do usuario
-    user = serializers.StringRelatedField()
+    # user = serializers.StringRelatedField()
+
+    # instância de um serializer - GET
+    user = UserSerializer(read_only=True)
+
+    # exibir apenas para escrita - POST/PUT
+    user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(),
+                                                 write_only=True,
+                                                 # nome do campo em models
+                                                 source='user')
 
     class Meta:
         model = Category
-        fields = ['id', 'name', 'description', 'user']
+        fields = ['id', 'name', 'description', 'user', 'user_id']
         # mostrar todos
         # fields = '__all__'
