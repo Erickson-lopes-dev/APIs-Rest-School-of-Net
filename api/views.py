ff48651fdd59a69f63608a4bec9d3d9a353c662a
EricksonLopes <ofc.erickson@gmail.com>
2020-08-10 15:35:13 -0300

    Serializers - ViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api.models import Category
+from api.serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
